50ea3a1f076341c559f5de9e6b41d7d275b07a18
Graham Corcoran <15987547+GrahamCorcoran@users.noreply.github.com>
2021-05-05 10:35:11 -0400

    Added logging and client info to global scope.

--- laukopier.py
+++ laukopier.py
@@ -4,40 +4,42 @@ from dotenv import dotenv_values
 import logging
 import logging.handlers
 
+config = dotenv_values(".env")
 
-def initialize():
-    config = dotenv_values(".env")
-
-    log = logging.getLogger("bot")
-    log.setLevel(logging.INFO)
-    log_formatter = logging.Formatter('%(levelname)s: %(message)s')
-    log_formatter_file = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    log_std_err_handler = logging.StreamHandler()
-    log_std_err_handler.setFormatter(log_formatter)
-    log.addHandler(log_std_err_handler)
-    if config["LOG_FILENAME"] is not None:
-        log_file_handler = logging.handlers.RotatingFileHandler(
-            config["LOG_FILENAME"],
-            maxBytes=int(config["LOG_FILE_MAXSIZE"]),
-            backupCount=int(config["LOG_FILE_BACKUPCOUNT"])
-        )
-        log_file_handler.setFormatter(log_formatter_file)
-        log.addHandler(log_file_handler)
-
-
-    reddit = praw.Reddit(
-        client_id=config["CLIENT_ID"],
-        client_secret=config["CLIENT_SECRET"],
-        password=config["PASSWORD"],
-        user_agent=config["USER_AGENT"],
-        username=config["USERNAME"]
+log = logging.getLogger("bot")
+log.setLevel(logging.INFO)
+log_formatter = logging.Formatter('%(levelname)s: %(message)s')
+log_formatter_file = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+log_std_err_handler = logging.StreamHandler()
+log_std_err_handler.setFormatter(log_formatter)
+log.addHandler(log_std_err_handler)
+if config["LOG_FILENAME"] is not None:
+    log_file_handler = logging.handlers.RotatingFileHandler(
+        config["LOG_FILENAME"],
+        maxBytes=int(config["LOG_FILE_MAXSIZE"]),
+        backupCount=int(config["LOG_FILE_BACKUPCOUNT"])
     )
+    log_file_handler.setFormatter(log_formatter_file)
+    log.addHandler(log_file_handler)
 
-    return reddit, log
+client = praw.Reddit(
+    client_id=config["CLIENT_ID"],
+    client_secret=config["CLIENT_SECRET"],
+    password=config["PASSWORD"],
+    user_agent=config["USER_AGENT"],
+    username=config["USERNAME"]
+)
+
+
+def valid_submission(client, submission):
+    # Self posts in BOLA never need to be copied.
+    if submission.is_self:
+        return False
+
+    url = submission.url
 
 
 def main():
-    client, log = initialize()
     bola = client.subreddit("bestoflegaladvice")
     while True:
         try:
