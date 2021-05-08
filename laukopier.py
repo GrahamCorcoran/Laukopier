ad4ad4c5b72be70f37e2214ddaff71ed07133b0b
Graham Corcoran <15987547+GrahamCorcoran@users.noreply.github.com>
2021-05-08 17:27:49 -0400

    Changed logging to loguru.

--- laukopier.py
+++ laukopier.py
@@ -1,28 +1,12 @@
 import time
 import praw
+from prawcore import exceptions
 from dotenv import dotenv_values
-import logging
-import logging.handlers
+from loguru import logger
 
 config = dotenv_values(".env")
 ignored_subreddits = ["legaladvice", "legaladviceofftopic"]
-
-# Logging info.
-log = logging.getLogger("bot")
-log.setLevel(logging.INFO)
-log_formatter = logging.Formatter('%(levelname)s: %(message)s')
-log_formatter_file = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-log_std_err_handler = logging.StreamHandler()
-log_std_err_handler.setFormatter(log_formatter)
-log.addHandler(log_std_err_handler)
-if config["LOG_FILENAME"] is not None:
-    log_file_handler = logging.handlers.RotatingFileHandler(
-        config["LOG_FILENAME"],
-        maxBytes=int(config["LOG_FILE_MAXSIZE"]),
-        backupCount=int(config["LOG_FILE_BACKUPCOUNT"])
-    )
-    log_file_handler.setFormatter(log_formatter_file)
-    log.addHandler(log_file_handler)
+logger.add("laukopier.log")
 
 client = praw.Reddit(
     client_id=config["CLIENT_ID"],
@@ -50,33 +34,41 @@ def format_message(self_text, title):
     for element in body:
         newbody.append("\n \n> " + element)
 
+    body = "".join(newbody)
+
     header = "**Reminder:** Do not participate in threads linked here." \
              " If you do, you may be banned from both subreddits." \
              "\n\n --- \n\n" \
              "Title: " + title + "\n\n" + "Body: \n\n"
     footer = "\n\n This bot was created to capture threads missed by LocationBot" \
              " and is not affiliated with the mod team." \
-             "\n\n [Concerns? Bugs?](https://www.reddit.com/message/compose/?to=laukopier)" \
-             " | [GitHub](https://github.com/GrahamCorcoran/Laukopier)"
+             "\n\n [Concerns? Bugs?](https://www.reddit.com/message/compose/?to=GrahamCorcoran)" \
+             " | [Laukopier 2.0](https://github.com/GrahamCorcoran/Laukopier)"
 
-    return f"{header}{newbody}{footer}"
+    return f"{header}{body}{footer}"
 
 
 def main():
+    logger.info("Started successfully.")
     bola = client.subreddit("bestoflegaladvice")
+
     while True:
         try:
             for submission in bola.stream.submissions(skip_existing=True):
                 target = client.submission(url=submission.url)
                 if valid_submission(submission, target):
-                    message = format_message(target.self_text, target.title)
+                    message = format_message(target.selftext, target.title)
+                    submission.reply(message)
+                    logger.info(f"Replied: {target.title}")
 
         except KeyboardInterrupt:
             break
+        except exceptions.PrawcoreException:
+            logger.exception("Sleep for 60 seconds.")
+            time.sleep(60)
         except Exception as e:
-            log.error("Exception %s", e, exc_info=True)
-            log.info("Sleep for 15 seconds.")
-            time.sleep(15)
+            logger.critical("PROGRAM FAILED")
+            raise e
 
 
 if __name__ == "__main__":
